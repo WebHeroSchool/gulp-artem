9163435ce01c88216d53f65922614f1fbb64a2c8
Artem <dunaev.aa@yandex.ru>
2018-12-21 15:19:34 +0500

    create simple gulp pipe

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 to run this script type in your terminal:
 
-"gulp getTime"
+"gulp css"      - to move css from 'dev' to 'prod'
+"gulp js"       - to move js from 'dev' to 'prod'
+"gulp transfer" - to move css and js from 'dev' to 'prod'
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require("gulp");
 
-gulp.task("getTime", () => {
-  const time = new Date();
-  console.log('\n current time is: ' + time.toLocaleTimeString() + '\n');
+gulp.task("css", () => {
+  return gulp.src("dev/css/*.css").pipe(gulp.dest("prod/css"));
 });
+
+gulp.task("js", () => {
+  return gulp.src("dev/js/*.js").pipe(gulp.dest("prod/js"));
+});
+
+gulp.task("transfer", ["css", "js"]);
