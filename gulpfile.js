27637998eba64075c74d53a02ccdb3f00216ae61
Artem <dunaev.aa@yandex.ru>
2018-12-24 11:58:48 +0500

    add css minify and concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,16 @@ const gulp = require("gulp");
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
 
-gulp.task("css", () => {
+gulp.task("build-css", () => {
   return gulp.src("dev/css/*.css")
+    .pipe(concat('styles.css'))
+    .pipe(cssnano())
     .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("js", () => {
+gulp.task("build-js", () => {
   return gulp.src("dev/js/*.js")
     .pipe(concat('index.js'))
     .pipe(babel({
@@ -18,4 +21,4 @@ gulp.task("js", () => {
     .pipe(gulp.dest("build/js"));
 });
 
-gulp.task("build", ["css", "js"]);
+gulp.task("build", ["build-css", "build-js"]);
